8a37b69e657591db20d78bac1ba2213250469afc
Nathan Schor <nate.schor@gmail.com>
2022-03-01 20:58:37 -0500

    Call other scripts in pipeline

--- code/00_run_pipeline.R
+++ code/00_run_pipeline.R
@@ -3,3 +3,9 @@ pacman::p_load(tidyverse,
                tidytext,
                sentimentr,
                topicmodels)
+
+source("code/01_calculate_sentiment.R")
+source("code/02_remove_stop_words.R")
+source("code/03_calculate_tf_idf.R")
+source("code/04_run_topic_model.R")
+
