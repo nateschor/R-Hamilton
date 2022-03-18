af27ed4fd5c2397ea3eee0e2d804809eb66a6a35
Nathan Schor <nate.schor@gmail.com>
2022-03-17 21:22:14 -0400

    Functionalize code

--- code/02_remove_stop_words.R
+++ code/02_remove_stop_words.R
@@ -1,23 +1,25 @@
 
-df_SMART_stop_words <- stop_words %>%
-  filter(lexicon == "SMART") 
+Choose_Stopwords <- function(char_lexicon) {
+  
+  stop_words %>% 
+    filter(lexicon == char_lexicon)
+  
+}
 
-df_onix_stop_words <- stop_words %>%
-  filter(lexicon == "onix") 
+df_SMART_stop_words <- Choose_Stopwords("SMART") 
+df_onix_stop_words <- Choose_Stopwords("onix") 
+df_snowball_stop_words <- Choose_Stopwords("snowball") 
 
-df_snowball_stop_words <- stop_words %>%
-  filter(lexicon == "snowball") 
+Remove_Stopwords <- function(df_stop_words) {
+  
+  df_tokenized %>% 
+    anti_join(., df_stop_words, by = "word")
+  
+}
 
-df_SMART_filtered <- df_tokenized %>% 
-  anti_join(., df_SMART_stop_words, by = "word")
-
-df_onix_filtered <- df_tokenized %>% 
-  anti_join(., df_onix_stop_words, by = "word")
-
-df_snowball_filter <- df_tokenized %>% 
-  anti_join(., df_snowball_stop_words, by = "word")
-
-df_all_lexicons_filtered <- df_tokenized %>% 
-  anti_join(., stop_words, by = "word")
+df_SMART_filtered <- Remove_Stopwords(df_SMART_stop_words)
+df_onix_filtered <- Remove_Stopwords(df_onix_stop_words)
+df_snowball_filter <- Remove_Stopwords(df_snowball_stop_words)
+df_all_lexicons_filtered <- Remove_Stopwords(stop_words)
 
 write_csv(df_all_lexicons_filtered, "data/intermediate/tokenized_and_stop_words_removed.csv")
